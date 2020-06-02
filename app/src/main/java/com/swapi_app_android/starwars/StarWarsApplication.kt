9d4a4db2ab001a05d743f321df323a825e7d006e
matheus <barros.matheusprestes@gmail.com>
2020-06-02 19:35:45 -0400

    Injeção de dependencias no modulo app e modificando arquitetura
    Signed-off-by: matheus <barros.matheusprestes@gmail.com>

--- app/src/main/java/com/swapi_app_android/starwars/StarWarsApplication.kt
+++ app/src/main/java/com/swapi_app_android/starwars/StarWarsApplication.kt
@@ -5,11 +5,18 @@ import com.swapi_app_android.starwars.dependencies.BaseDependencies
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
+
 class StartWarsApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        initDI()
+    }
+
+    private fun initDI() {
         startKoin {
+            // Android context
             androidContext(this@StartWarsApplication)
+            // modules
             modules(BaseDependencies.networkingModule)
         }
     }
---
+++ app/src/main/java/com/swapi_app_android/starwars/archieteture/RemoteReponse.kt
@@ -0,0 +1,11 @@
+package com.swapi_app_android.starwars.archieteture
+
+import com.google.gson.annotations.SerializedName
+
+
+data class RemoteResponse<T>(
+    @SerializedName("count") val count: Int,
+    @SerializedName("next") val next: String?,
+    @SerializedName("previous") val previous: String?,
+    @SerializedName("results") val results: T?
+)
