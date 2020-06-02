8c52f8f0d2d9cb229658e3828c00bd8ab29aa7b0
matheus <barros.matheusprestes@gmail.com>
2020-06-02 18:13:18 -0400

    Commit incial: Organizando os modulos do projeto
    Signed-off-by: matheus <barros.matheusprestes@gmail.com>

---
+++ app/src/main/java/com/swapi_app_android/starwars/StarwarsMainActivity.kt
@@ -0,0 +1,12 @@
+package com.mbarros64.swapi_app_android
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class StarwarsMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_starwars_main)
+    }
+}
